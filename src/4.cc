c9460a99881c047f08ec29608fac4a7726bc28ef
yamanobori_old <yamanobori_old@yahoo.co.jp>
2016-04-12 21:14:55 +0900

    lambda 式．

---
+++ src/4.cc
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    // 初期化子
+    std::vector<std::string> v { "January", "February", "March", "April", "May",
+    "June", "July", "August", "September", "October", "November", "December" };
+
+    for_each(v.begin(), v.end(), [](const std::string& s) { std::cout << s << std::endl;} );
+
+    return 0;
+}
